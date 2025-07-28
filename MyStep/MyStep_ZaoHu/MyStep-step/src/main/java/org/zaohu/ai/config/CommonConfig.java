f62e3afd1e0f4b150c7b245478a8d3f61d256575
铁汉柔情 <3331247124@qq.com>
2025-07-29 04:03:18 +0800

    ollama简易代码

---
+++ MyStep/MyStep_ZaoHu/MyStep-step/src/main/java/org/zaohu/ai/config/CommonConfig.java
@@ -0,0 +1,19 @@
+package org.zaohu.ai.config;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonConfig {
+    @Autowired
+    private OpenAiChatModel model;
+
+//    @Bean
+//    public ConsultantService consultantService(){
+//        ConsultantService build = AiServices.builder(ConsultantService.class)
+//                .chatModel(model)
+//                .build();
+//        return build;
+//    }
+}
