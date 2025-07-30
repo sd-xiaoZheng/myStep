16f6d01143e8951691199100ed6310128966372b
sd-xiaoZheng <3331247124@qq.com>
2025-07-30 22:55:20 +0800

    init

--- MyStep/MyStep_ZaoHu/MyStep-step/src/main/java/org/zaohu/ai/config/CommonConfig.java
+++ MyStep/MyStep_ZaoHu/MyStep-step/src/main/java/org/zaohu/ai/config/CommonConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
-    @Autowired
-    private OpenAiChatModel model;
+//    @Autowired
+//    private OpenAiChatModel model;
 
 //    @Bean
 //    public ConsultantService consultantService(){
--- MyStep/MyStep_ZaoHu/MyStep-step/src/main/java/org/zaohu/ai/service/ConsultantService.java
+++ MyStep/MyStep_ZaoHu/MyStep-step/src/main/java/org/zaohu/ai/service/ConsultantService.java
@@ -3,10 +3,10 @@ package org.zaohu.ai.service;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
-@AiService(
-        wiringMode = AiServiceWiringMode.EXPLICIT,//手动装配模型 也可以自动 会自动去ioc模型里面去找一个
-        chatModel = "openAiChatModel"//指定模型,
-)
+//@AiService(
+//        wiringMode = AiServiceWiringMode.EXPLICIT,//手动装配模型 也可以自动 会自动去ioc模型里面去找一个
+//        chatModel = "openAiChatModel"//指定模型,
+//)
 //@AiService
 public interface ConsultantService {
     //用于聊天的方法
