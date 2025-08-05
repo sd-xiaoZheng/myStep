7321de6fa624bc29c0564714a8f71b127adcebde
铁汉柔情 <3331247124@qq.com>
2025-08-05 20:02:16 +0800

    更新一下数据库文件

--- MyStep/MyStep_ZaoHu/MyStep-step/src/main/java/org/zaohu/ai/service/ConsultantService.java
+++ MyStep/MyStep_ZaoHu/MyStep-step/src/main/java/org/zaohu/ai/service/ConsultantService.java
@@ -1,9 +1,6 @@
 package org.zaohu.ai.service;
 
-import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.TokenStream;
-import dev.langchain4j.service.spring.AiService;
-import dev.langchain4j.service.spring.AiServiceWiringMode;
 
 //@AiService(
 //        wiringMode = AiServiceWiringMode.EXPLICIT,//手动装配模型 也可以自动 会自动去ioc模型里面去找一个
